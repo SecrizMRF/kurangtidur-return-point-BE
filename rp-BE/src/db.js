9e3bce5f44240861829f0ef3701dc4415e5bc237
SecrizMRF <secrizkyfadhillah@gmail.com>
2025-11-30 19:50:46 +0700

    update

--- rp-BE/src/db.js
+++ rp-BE/src/db.js
@@ -2,12 +2,17 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT || 5432,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = {
